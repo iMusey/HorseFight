636304cb91d346209ad5c4ae2c555ae014181c6d
iMusey <alexander.farahany@gmail.com>
2025-04-25 18:22:46 -0500

    horse

--- Assets/HorseScript.cs
+++ Assets/HorseScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HorseScript : MonoBehaviour
 {
+    public float speed;
+    public float maxHealth;
+    public float strength;
+
+    public Vector2 facing;
+
     // Start is called before the first frame update
     void Start()
     {
